60f2734a6e41b5b0ea2bf6e34d2d47fe572039a9
João <jpgon@live.com.pt>
2023-03-08 12:45:00 +0000

    3rd try: changing service worker to cache game files

--- service-worker.js
+++ service-worker.js
@@ -25,7 +25,7 @@ self.addEventListener("fetch", function (event) {
 });
  */
 
-importScripts(
+/* importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
@@ -47,5 +47,71 @@ self.addEventListener("message", (event) => {
         return cache.addAll(event.data.payload);
       })
     );
+  }
+}); */
+
+const cacheName = "farmingio_v1";
+
+const precachedAssets = [
+  '/index.html',
+  '/style.css',
+  '/service-worker.js',
+  '/scripts/game.js',
+  '/scripts/gameAreas.js',
+  '/scripts/gameSave.js',
+  '/scripts/gui.js',
+  '/scripts/player.js',
+  '/scripts/quests.js',
+  '/scripts/variables.js',
+  '/scripts/worldMap.js',
+  '/scripts/controls/joystick.js',
+  '/scripts/controls/controls.js',
+  '/assets/images/iconBook.png',
+  '/assets/images/iconChest.png',
+  '/assets/images/iconPrizes.png',
+  '/assets/images/iconProfile.png',
+  '/assets/images/openedBook.png',
+  '/assets/map/collisions.js',
+  '/assets/map/collisionsArray.js',
+  '/assets/map/farming_io_map.png',
+  '/assets/map/gardenArray.js',
+  '/assets/sprites/cultivated.png',
+  '/assets/sprites/cultivated2.png',
+  '/assets/sprites/cultivated3.png',
+  '/assets/sprites/playerDown.png',
+  '/assets/sprites/playerLeft.png',
+  '/assets/sprites/playerRight.png',
+  '/assets/sprites/playerUp.png',
+  '/assets/sprites/hoeImg.png',
+  '/assets/sprites/plowedLand.png',
+  '/assets/sprites/resetBlock.png',
+  '/assets/sprites/seedsImg.png',
+  '/assets/sprites/sickleImg.png',
+  '/assets/sprites/waterImg.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    return cache.addAll(precachedAssets);
+  }));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      caches.open(cacheName).then((cache) => {
+        return fetch(event.request.url)
+          .then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
+
+            return fetchedResponse;
+          })
+          .catch(() => {
+            return cache.match(event.request.url);
+          });
+      })
+    );
+  } else {
+    return;
   }
 });
