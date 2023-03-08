9a78e9d2491a49aaedb39ae69c042001ca2c363d
João <jpgon@live.com.pt>
2023-03-08 12:14:46 +0000

    changing service worker to cache game files

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,20 @@ workbox.routing.registerRoute(
   ({ request }) => request.destination === "image",
   new workbox.strategies.NetworkFirst()
 );
+
+self.addEventListener("install", function (event) {
+  caches.open("PRECACHE").then(function (cache) {
+    cache.addAll(cacheList);
+  });
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
