f6e2a9e055b830908e4891acac39b2e2c03bc65b
João <jpgon@live.com.pt>
2023-03-09 09:52:31 +0000

    feat: play offline 100% working

--- service-worker.js
+++ service-worker.js
@@ -35,6 +35,7 @@ let coreAssets = [
   "/index.html",
 ];
 
+//cache game files
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("app").then(function (cache) {
@@ -46,17 +47,18 @@ self.addEventListener("install", function (event) {
   );
 });
 
+//get game files when offline
 self.addEventListener("fetch", function (event) {
-  console.log("Fetch event for ", event.request.url);
+  /* console.log("Fetch event for ", event.request.url) */
   event.respondWith(
     caches
       .match(event.request)
       .then(function (response) {
         if (response) {
-          console.log("Found ", event.request.url, " in cache");
+          /* console.log("Found ", event.request.url, " in cache") */
           return response;
         }
-        console.log("Network request for ", event.request.url);
+        /* console.log("Network request for ", event.request.url) */
         return fetch(event.request);
       })
       .catch(function (error) {
@@ -64,173 +66,3 @@ self.addEventListener("fetch", function (event) {
       })
   );
 });
-
-/* self.addEventListener("fetch", function (event) {
-  let request = event.request;
-  if (
-    event.request.cache === "only-if-cached" &&
-    event.request.mode !== "same-origin"
-  )
-    return;
-
-  // HTML files
-  // Network-first
-  if (request.headers.get("Accept").includes("text/html")) {
-    event.respondWith(
-      fetch(request)
-        .then(function (response) {
-          let copy = response.clone();
-          event.waitUntil(
-            caches.open("app").then(function (cache) {
-              return cache.put(request, copy);
-            })
-          );
-          return response;
-        })
-        .catch(function (error) {
-          return caches.match(request).then(function (response) {
-            return response || caches.match("/offline.html");
-          });
-        })
-    );
-  }
-
-  // CSS & JavaScript
-  // Offline-first
-  if (
-    request.headers.get("Accept").includes("text/css") ||
-    request.headers.get("Accept").includes("text/javascript")
-  ) {
-    event.respondWith(
-      caches.match(request).then(function (response) {
-        return (
-          response ||
-          fetch(request).then(function (response) {
-            return response;
-          })
-        );
-      })
-    );
-    return;
-  }
-
-  // Images
-  // Offline-first
-  if (request.headers.get("Accept").includes("image")) {
-    event.respondWith(
-      caches.match(request).then(function (response) {
-        return (
-          response ||
-          fetch(request).then(function (response) {
-            let copy = response.clone();
-            event.waitUntil(
-              caches.open("app").then(function (cache) {
-                return cache.put(request, copy);
-              })
-            );
-            return response;
-          })
-        );
-      })
-    );
-  }
-}); */
-
-/* ---------------------------------------------------------------- */
-
-/* // On install, cache core assets
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open("app").then(function (cache) {
-      for (let asset of coreAssets) {
-        cache.add(new Request(asset));
-      }
-      return cache;
-    })
-  );
-});
-
-// Listen for request events
-self.addEventListener("fetch", function (event) {
-  let request = event.request;
-  if (
-    event.request.cache === "only-if-cached" &&
-    event.request.mode !== "same-origin"
-  )
-    return;
-
-  // HTML files
-  // Network-first
-  if (
-    request.headers.get("Accept").includes("text/html") ||
-    request.headers.get("Accept").includes("text/css") ||
-    request.headers.get("Accept").includes("text/javascript") ||
-    request.headers.get("Accept").includes("image")
-  ) {
-    event.respondWith(
-      fetch(request)
-        .then(function (response) {
-          // Create a copy of the response and save it to the cache
-          let copy = response.clone();
-
-          caches.open("app").then(function (cache) {
-            return cache.put(request, copy);
-          });
-
-          // Return the response
-          return response;
-        })
-        .catch(function (error) {
-          // If there's no item in cache, respond with a fallback
-          return caches.match(request).then(function (response) {
-            return response || caches.match("/offline.html");
-          });
-        })
-    );
-  }
-
-  // CSS & JavaScript
-  // Offline-first
-  if (
-    request.headers.get("Accept").includes("text/css") ||
-    request.headers.get("Accept").includes("text/javascript")
-  ) {
-    event.respondWith(
-      caches.match(request).then(function (response) {
-        return (
-          response ||
-          fetch(request).then(function (response) {
-            // Return the response
-            return response;
-          })
-        );
-      })
-    );
-    return;
-  }
-
-  // Images
-  // Offline-first
-  if (request.headers.get("Accept").includes("image")) {
-    event.respondWith(
-      caches.match(request).then(function (response) {
-        return (
-          response ||
-          fetch(request).then(function (response) {
-            // Save a copy of it in cache
-            let copy = response.clone();
-            event.waitUntil(
-              caches.open("app").then(function (cache) {
-                return cache.put(request, copy);
-              })
-            );
-
-            // Return the response
-            return response;
-          })
-        );
-      })
-    );
-  }
-});
- */
